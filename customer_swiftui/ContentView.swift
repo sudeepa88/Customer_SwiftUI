1ed49afa4e7ecf7059c19a8231116ecbbb47ef42
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-20 22:38:22 +0530

    Initial Commit

---
+++ customer_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  customer_swiftui
+//
+//  Created by sudeepa pal on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ customer_swiftui/customer_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  customer_swiftuiApp.swift
+//  customer_swiftui
+//
+//  Created by sudeepa pal on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct customer_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
